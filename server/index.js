ce965de54421856103e7def827704a60e250da6a
Daniel Yovanda <danielyovanda@Daniels-MacBook-Air.local>
2025-11-08 15:52:23 +0700

    Fix CORS preflight handling

--- server/index.js
+++ server/index.js
@@ -30,7 +30,10 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
 }));
 app.use(bodyParser.json());
 
